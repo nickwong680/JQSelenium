4e59510f84162f3a9c6a42303243ee9665fdc4ea
Byron Sommardahl <byron@acklenavenue.com>
2013-05-24 13:35:52 -0500

    Upgraded selenium to newest version.

---
+++ src/JQSelenium.Knockout/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JQSelenium.Knockout")]
+[assembly: AssemblyDescription("Knockout helpers for JQSelenium, the C# wrapper of jQuery for Selenium WebDriver.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acklen Avenue")]
+[assembly: AssemblyProduct("JQSelenium.Knockout")]
+[assembly: AssemblyCopyright("Copyright © Acklen Avenue, LLC 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d5e41af9-9363-42be-8492-9c39d67b8c60")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
